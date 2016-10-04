f5aeb75c59de2c25fd66a9c9d87d38e4b505cf98
NuclearCoder <hd85.ms@gmail.com>
2016-10-04 15:56:10 +0200

    Makefile and header reorder

--- neural-net/headers/header.h
+++ neural-net/headers/header.h
@@ -4,13 +4,14 @@ typedef float float_t;
 typedef unsigned int uint_t;
 typedef float_t(*floatfun_t)(float_t); // should be ( R -> [-1;1] ) most of the time
 
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <random>
+#include <vector>
+
 #include "matrix.h"
 #include "gaussian.h"
 
 #include "activation.h"
 #include "network.h"
-
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <random>
